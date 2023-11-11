d54a94e46b926f86247cee351be36b6ce1ac6cb8
kpbaks <kristoffer.pbs@tuta.io>
2023-11-11 16:02:57 +0100

    feat: convert to new format

---
+++ tests/tests.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "duration.h"
+
+// You can make your own test cases in this file for the Duration class.
+// Below is an example test case.
+// TEST_CASE is a macro that creates a test case.
+// REQUIRE is a macro that checks if the expression is true. Similar to assert.
+
+TEST_CASE("example") {
+    REQUIRE(1 == 1);
+}
+
